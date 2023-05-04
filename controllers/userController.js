ca90442b90848109851ba4fdb9d21839ebfa2cf6
AmalStudy <amalengstudy@gmail.com>
2023-05-04 00:37:37 -0700

    update controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+
     // Get all users
+
     getUsers (req, res) {
         User.find()
             .then((users) => {
@@ -12,7 +14,9 @@ module.exports = {
                 return res.status(500).json(err);
             });
     },
+
     // Get a single user by its _id and populated thought and friend data
+
     getSingleUser (req, res) {
         User.findOne({ _id: req.params.userId })
             .select('-__v')
@@ -28,7 +32,9 @@ module.exports = {
                 return res.status(500).json(err);
             });
     },
+
     // Create a new user
+
     createUser (req, res) {
         User.create(req.body)
             .then((user) => {
@@ -38,7 +44,9 @@ module.exports = {
                 return res.status(500).json(err)
             });
     },
+
     // Update a user by its _id
+    
     updateUser (req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
@@ -58,14 +66,18 @@ module.exports = {
                 return res.status(500).json(err);
             });
     },
+
     // Delete a user by its _id
+
     deleteUser (req, res) {
         User.findOneAndDelete({ _id: req.params.userId })
             .then((user) => {
                 if (!user) {
                     res.status(404).json({ message: 'No user with this id found' })
                 }
+
                 // Remove a user's associated thoughts when deleted
+
                 Thought.deleteMany({ _id: { $in: user.thoughts } })
                 return res.json({
                     deletedUser: user,
@@ -79,6 +91,7 @@ module.exports = {
     },
 
     // Add a new friend to a user's friend list
+
     addFriend (req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
@@ -97,7 +110,9 @@ module.exports = {
                 return res.status(500).json(err)
             });
     },
+
     // Remove a friend from a user's friend list
+    
     removeFriend (req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
